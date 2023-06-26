5f76f475e2bf3e5db648e1a5fd917292a7ed331a
정주호 <meju7015@gmail.com>
2023-06-26 15:33:44 +0900

    Created README

---
+++ README.md
@@ -0,0 +1,6 @@
+## Speak My GPT
+다정이의 다양한 질문에 대응하기위한 스피커봇
+
+## Service
+- AWS Polly
+- OpenAI
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
 import SpeechToText from "./openAI/SpeechToText";
